d95085430956f72516f3974062353b45e6d5e01f
Jake Ninness <jake.ninness@gmail.com>
2016-08-27 11:06:31 +1200

    files added to commit

--- js/script.js
+++ js/script.js
@@ -24,22 +24,28 @@ new Vue({
         active: false
       },
       {
-        name: 'Number of site page: 1 - 3',
+        name: 'Number of site pages: 1 - 3',
         price: 400,
         active: false
       },
       {
-        name: 'Number of site page: 4 - 6',
+        name: 'Number of site pages: 4 - 6',
         price: 400,
         active: false
       },
       {
-        name: 'Number of site page: 7 +',
+        name: 'Number of site pages: 7 +',
         price: 400,
         active: false
       },
     ]
+  },
+  methods: {
+    toggleActive: function(s) {
+      s.active = !s.active;
+    }
 
   }
+
 
 });
