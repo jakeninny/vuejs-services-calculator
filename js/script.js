eea1b898c50532f7afca2a2fa9188caeff06cad6
Jake Ninness <jake.ninness@gmail.com>
2016-08-27 10:52:22 +1200

    got services listing from vue data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,45 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Testing testing 123'
+    //list services as array of objects
+    services: [
+      {
+        name: 'Website Design',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Branding',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Logo Design',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Website Development',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Number of site page: 1 - 3',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Number of site page: 4 - 6',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Number of site page: 7 +',
+        price: 400,
+        active: false
+      },
+    ]
+
   }
+
 });
-
-
-console.log('test');
